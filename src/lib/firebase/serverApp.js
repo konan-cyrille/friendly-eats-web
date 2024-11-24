f279f73b19989688895f123e42bbf03e2a7698fd
it momaii <cdibi@momaii.com>
2024-11-24 14:08:23 +0100

    update auth feature

--- src/lib/firebase/serverApp.js
+++ src/lib/firebase/serverApp.js
@@ -20,8 +20,6 @@ export async function getAuthenticatedAppForUser() {
       : {}
   );
 
-  console.log("-------------------")
-
   const auth = getAuth(firebaseServerApp);
   await auth.authStateReady();
 
